fdd4b63d017a4024cbcc15dcf285275cca5ba14d
Fernando C <cavalcantefernandoht@gmail.com>
2020-06-22 21:22:05 -0300

    App

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -7,6 +7,8 @@ import { confirmAlert } from 'react-confirm-alert';
 import axios from 'axios';
 import api from '../../services/api';
 
+import Dropzone from '../../components/Dropzone';
+
 import './styles.css';
 import 'react-confirm-alert/src/react-confirm-alert.css';
 
@@ -48,6 +50,7 @@ const CreatePoint = () => {
   const [selectedCity, setSelectedCity] = useState('0');
   const [selectedItems, setSelectedItems] = useState<number[]>([]);
   const [selectedPosition, setSelectedPosition] = useState<[number, number]>([0, 0]);
+  const [selectedFile, setSelectedFile] = useState<File>();
 
   const history = useHistory();
 
@@ -132,17 +135,20 @@ const CreatePoint = () => {
       });
     } else {
 
+      const data = new FormData();
 
-      const data = {
-        name,
-        email,
-        whatsapp,
-        uf,
-        city,
-        latitude,
-        longitude,
-        items
-      };
+      data.append('name', name);
+      data.append('email', email);
+      data.append('whatsapp', whatsapp);
+      data.append('uf', uf);
+      data.append('city', city);
+      data.append('latitude', String(latitude));
+      data.append('longitude', String(longitude));
+      data.append('items', items.join(','));
+      if (selectedFile) {
+        data.append('image', selectedFile);
+      }
+
 
       try {
         await api.post('points', data);
@@ -209,6 +215,9 @@ const CreatePoint = () => {
       </header>
       <form onSubmit={handleSubmit}>
         <h1>Cadastro do <br /> ponto de coleta</h1>
+        <Dropzone
+          onFileUploaded={setSelectedFile}
+        />
         <fieldset>
           <legend>
             <h2>Dados</h2>
